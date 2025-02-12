dbb61e2ba2e0fe88bd0f4815deed5ccab4c773cb
abdulaziz2706 <abdulaziz27707@gmail.com>
2025-02-12 16:12:50 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+let randomSon = Math.round(Math.random()*100)
+
+console.log(randomSon);
+
+alert(`hozir sizga random sonlar ciqadi`)
+
+alert(`sizning random soningiz: ${randomSon}
+    Random son  ikki marta kattalashsa u : ${randomSon} * 2 = ${randomSon*2}
+    Random son  ikki marta kattalashsa u : ${randomSon} / 2 = ${randomSon/2}
+    Random son  3 ga bo'lgandandagi qoldiq: ${randomSon % 3}
+    `)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
